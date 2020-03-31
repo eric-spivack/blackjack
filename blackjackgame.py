46ec96b9d291ac531a7f0fbe80a5460958c3fc5f
Eric Spivack <eric_spivack@mednax.com>
2020-03-31 15:42:33 -0400

    added new_deck and deal

---
+++ blackjackgame.py
@@ -0,0 +1,4 @@
+from deck import Deck
+
+deck = Deck()
+print(deck.cards)
--- deck.py
+++ deck.py
@@ -1,9 +1,20 @@
+from card import Card
+
 class Deck:
 
     def __init__(self):
         self.cards = []
+        self.suits = ["clubs", "diamonds", "spades", "hearts"]
+        self.values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        self.new_deck()
 
     def new_deck(self):
         self.cards = []
-        suits = ["clubs", "diamonds", "spades", "hearts"]
-        values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+        for suit in self.suits:
+            for value in self.values:                
+                card = Card(suit, value)
+                self.cards.append(card)
+
+    def deal(self):
+        return self.cards[0]
