c303f923ca4766d57c341f1cdb73aecc9899903b
tpolk-mednax <59485819+tpolk-mednax@users.noreply.github.com>
2020-04-28 15:48:00 -0400

    Co-authored-by: Karl Hewling <Karl_Hewling@mednax.com> Co-authored-by: Nina_Parton@mednax.com <Nina_Parton@mednax.com>

--- blackjackgame.py
+++ blackjackgame.py
@@ -1,6 +1,9 @@
 from player import Player
 from deck import Deck
 
+def winner_check(players):
+    print()
+
 deck = Deck()
 player = Player('Eric', 0)
 dealer = Player('Karl', 0)
@@ -16,3 +19,14 @@ dealer.hit(deck.deal())
 
 print(player.hand)
 print(dealer.hand)
+
+if player.hand_check(deck):
+    dealt_card = deck.deal() 
+    player.hit(dealt_card)
+      
+if dealer.hand_check(deck):
+    dealt_card = deck.deal()
+    dealer.hit(dealt_card)
+
+filtered_items = filter(lambda p: p.score() < 22, [player, dealer])
+winner_check(list(filtered_items))
