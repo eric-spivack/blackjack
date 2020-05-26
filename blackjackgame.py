cfd42894aef614df4f3e46ec3be3b0772553da5e
tpolk-mednax <59485819+tpolk-mednax@users.noreply.github.com>
2020-05-26 15:27:30 -0400

    Added Sort

--- blackjackgame.py
+++ blackjackgame.py
@@ -1,8 +1,9 @@
 from player import Player
 from deck import Deck
 
-def winner_check(players):
-    print()
+def winner_check(players, deck):
+    sorted_players = sorted(players, key=lambda p: p.score(deck), reverse=True)
+    print(sorted_players)
 
 deck = Deck()
 player = Player('Eric', 0)
@@ -28,5 +29,5 @@ if dealer.hand_check(deck):
     dealt_card = deck.deal()
     dealer.hit(dealt_card)
 
-filtered_items = filter(lambda p: p.score() < 22, [player, dealer])
-winner_check(list(filtered_items))
+filtered_items = filter(lambda p: p.score(deck) < 22, [player, dealer])
+winner_check(list(filtered_items), deck)
